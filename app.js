0f2d53db9024157d1be44d7c50457a7112e4ba8d
robine81 <robine.westberg@gmail.com>
2023-04-18 17:22:56 +0200

    All mandatory iterations done

--- app.js
+++ app.js
@@ -13,10 +13,26 @@ app.use(expressLayouts);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Add the route handlers here:
+
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+// Add the route handlers here:
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {res.render('beers.ejs', { beersFromApi })})
+  .catch(error => console.log(error));
+});
+
+// Add the route handlers here:
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(randomBeerFromApi => {
+    console.log(randomBeerFromApi) 
+    res.render('random-beer.ejs', { randomBeerFromApi })})
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
